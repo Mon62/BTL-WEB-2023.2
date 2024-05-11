67d0ad8ba098f84b868ae2276a6fc1337f91d776
Nam Nguyen Hai <nguyenhainam.work22@gmail.com>
2024-05-11 17:22:34 +0700

    added Story models

---
+++ frontend/src/models/Story.js
@@ -0,0 +1,9 @@
+export class Story {
+    constructor(username, caption, media, musicURL) {
+      this.username = username;
+      this.caption = caption;
+      this.media = media;
+      this.musicURL = musicURL;
+    }
+  }
+  
